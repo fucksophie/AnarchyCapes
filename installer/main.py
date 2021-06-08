c61ec549ab5ef06380320b4c1e9ac297a9ef7bf6
yourfriendoss <lukewarmcat@riseup.net>
2021-06-08 18:05:03 +0300

    Gitignore & Installer fix

--- installer/main.py
+++ installer/main.py
@@ -2,11 +2,9 @@ import os
 
 host = "C:\Windows\System32\drivers\etc\hosts"
 
-service = "None"
-
 from appJar import gui
 
-app = gui()
+app = gui("[AC]")
 
 # Check for administrator
 if os.name == "nt":
@@ -19,20 +17,12 @@ if os.name == "nt":
 	except:
 		app.infoBox("Noo..", "Start me as administrator!")
 		exit()
-	
 elif os.name == "posix":
 	host = "\\etc\\hosts"
 
 	if not os.getuid() == 0:
 		app.infoBox("Noo..", "Use sudo to start me.")
 		exit()
-
-#Define all needed functions
-def line_prepender(filename, line):
-    with open(filename, 'r+') as f:
-        content = f.read()
-        f.seek(0, 0)
-        f.write(line.rstrip('\r\n') + '\n' + content)
 
 def remove(file, word):
     dummy_file = file + '.bak'
@@ -52,43 +42,26 @@ def remove(file, word):
         os.remove(dummy_file)
 
 def handle(button):
-
-	global service
-	
-	#Handle services
-	if button == "CloaksPlus":
-		service = button
-		app.setLabel("selected", "Current: " + service)
-	if button == "AnarchyCapes":
-		service = button
-		app.setLabel("selected", "Current: " + service)
-
 	#Handle actions
 	if button == "Install":
 		remove(host, "s.optifine.net")
-		app.setLabel("selected", "Current: " + service)
-		app.infoBox("Yay!", "Installed " + service + "!")
-		if service == "AnarchyCapes":
-			line_prepender(host, "54.37.139.51 s.optifine.net")
-		elif service == "CloaksPlus":
-			line_prepender(host, "159.203.120.188 s.optifine.net")
+
+		with open(host, 'r+') as f:
+			content = f.read()
+			f.seek(0, 0)
+			f.write("54.37.139.51 s.optifine.net".rstrip('\r\n') + '\n' + content)
+
+		app.infoBox("Yay!", "Installed AnarchyCapes!")
 	elif button == "Uninstall":
 		remove(host, "s.optifine.net")
-		app.infoBox("Yay!", "Uninstalled all Services!")
+		app.infoBox("Yay!", "Uninstalled AnarchyCapes!!")
 	pass
 
 app.setBg("#2A2D32")
 app.setFg("#FFFFFF")
 app.setResizable(canResize=False)
 
-app.addLabel("title", "Cape Service Installer")
+app.addLabel("title", "AnarchyCapes Installer")
 
-app.addLabel("actions", "Actions")
 app.addButtons(["Uninstall", "Install"], handle)
-
-app.addLabel("services", "Services")
-app.addButton("AnarchyCapes", handle)
-app.addButton("CloaksPlus", handle)
-app.addLabel("selected", "Current: " + service)
-
 app.go()
